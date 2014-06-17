cad9c30eaa3f96eb2829aed679368b134bc8d379
Kleyson Prado <kleyson.prado@gmail.com>
2014-06-17 13:59:59 -0300

    Alterando o scopo para Request

--- src/br/com/controller/AlunoBean.java
+++ src/br/com/controller/AlunoBean.java
@@ -1,49 +1,55 @@
 package br.com.controller;
+
+import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 
 import br.com.dao.AlunoDAO;
 import br.com.model.Aluno;
 
 @ManagedBean
-@SessionScoped
-public class AlunoBean{
+@RequestScoped
+public class AlunoBean implements Serializable {
+
+	private static final long serialVersionUID = -8617449298454815446L;
+	private Aluno aluno = new Aluno();
+	private AlunoDAO dao = new AlunoDAO();
+	private List<Aluno> alunos;
+
+	// retorna os alunos do bacno
+	public List<Aluno> getAlunos() {
+		this.alunos = dao.listAlunos();
+		return alunos;
+	}
+
+	public Aluno getAluno() {
+		if (aluno == null) {
+			aluno = new Aluno();
+		}
+
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	// salva aluno no banco
+	public String salvaAluno() {
+		dao.save(aluno);
+		aluno = new Aluno();
+		return "listar?faces-redirect=true";
+	}
 	
+	public String updateAluno() {
+		return "add";
+	}
 
-		private Aluno aluno = new Aluno();
-		private AlunoDAO dao = new AlunoDAO();
-		private List<Aluno> alunos;
-		
-		//retorna os alunos do bacno
-		public List<Aluno> getAlunos() {
-			this.alunos = dao.listAlunos();
-			return alunos;
-		}
-
-		public Aluno getAluno() {
-			if (aluno == null){
-				aluno = new Aluno();
-			}
-			
-			return aluno;
-		}
-
-		public void setAluno(Aluno aluno) {
-			this.aluno = aluno;
-		}
-		
-		//salva aluno no banco
-		public String salvaAluno(){
-			dao.save(aluno);
-			aluno = new Aluno();
-			return "listar?faces-redirect=true";	
-		}
-		
-		public String excluir(){
-			dao.remove(aluno);
-			aluno = new Aluno();
-			return "listar?faces-redirect=true";
-		}
+	public String excluir() {
+		dao.remove(aluno);
+		aluno = new Aluno();
+		return "listar?faces-redirect=true";
+	}
 }
